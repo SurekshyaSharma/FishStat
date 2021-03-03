3525043cbb7e61d691b1e1caed4f5a75a98b59f9
SurekshyaSharma <shar3415@stthomas.edu>
2021-03-03 02:00:44 -0600

    refactor and created a dynamic array for fish species and lakes

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from collections import Counter
 
 
-# open and read the file after the appending:
+# open and read the file after the appending
 
 
 def readFile():
@@ -22,129 +22,89 @@ def readFile():
 
 def fishStatistic(lines_file):
     # Overall array of fish data
+    fish_data_species = dict()
+    fish_data_lake = dict()
     fish_data = []
     # For graphical representation
     fish_Species = []
-    location = []
+    lakes = []
     length = []
-    # fish Species
-    Walleye = []
-    White_Sucker = []
-    Yellow_Perch = []
-    Smallmouth_Bass = []
-    Northern_Pike = []
-    Sunfish = []
-    # location
-    itasca = []
-    vermilion = []
-    snail = []
-    superior = []
-    carlos = []
-    # total number of lines
+
     for i in lines_file:
         fish_Stat = i.split(",")
-        # creating the 2D
+        # creating the 2D 2D[[][]...]
         fish_data.append([fish_Stat[0], fish_Stat[1], fish_Stat[2]])
         # individual array for fish_Species, location and length for graphical representation
         fish_Species.append(fish_Stat[0])
-        location.append(fish_Stat[1])
-        length.append(fish_Stat[2])
-    print("Length:", Counter(fish_Species).items())
-    print("Location:", Counter(location).items())
+        lakes.append(fish_Stat[1])
+        length.append(int(fish_Stat[2]))
+        print(length)
+
+    # creating a dynamic empty array for different fish species
+    for i in Counter(fish_Species).items():
+        fish_data_species[i[0]] = []
+    # appending in an empty array of fish_data_species[i[0]] of different length of different fish species
     for i in fish_data:
-        # creating array for different length of different fish species
-        if i[0] == 'Walleye':
-            Walleye.append(int(i[2]))
-        if i[0] == 'White Sucker':
-            White_Sucker.append(int(i[2]))
-        if i[0] == 'Yellow Perch':
-            Yellow_Perch.append(int(i[2]))
-        if i[0] == 'Smallmouth Bass':
-            Smallmouth_Bass.append(int(i[2]))
-        if i[0] == 'Northern Pike':
-            Northern_Pike.append(int(i[2]))
-        if i[0] == 'Sunfish':
-            Sunfish.append(int(i[2]))
-        if i[1] == 'Itasca':
-            itasca.append(int(i[2]))
-        if i[1] == 'Vermilion':
-            vermilion.append(int(i[2]))
-        if i[1] == 'Snail':
-            snail.append(int(i[2]))
-        if i[1] == 'Superior':
-            superior.append(int(i[2]))
-        if i[1] == 'Carlos':
-            carlos.append(int(i[2]))
-    # creating 2D array
-    fish_species_average = [["Walleye", find_average(Walleye)],
-                            ["White Sucker", find_average(White_Sucker)],
-                            ["Yellow Perch", find_average(Yellow_Perch)],
-                            ["Smallmouth Bass", find_average(Smallmouth_Bass)],
-                            ["Northern Pike", find_average(Northern_Pike)],
-                            [" Sunfish", find_average(Sunfish)]]
+        fish_data_species[i[0]].append(int(i[2]))
+    # print(fish_data_species)
 
     # calculating the average of all species
-    print("1a.The average size of Walleye is", fish_species_average[0][1])
-    print("1b.The average size of White Sucker is", fish_species_average[1][1])
-    print("1c.The average size of Yellow Perch is", fish_species_average[2][1])
-    print("1d.The average size of Smallmouth Bass is", fish_species_average[3][1])
-    print("1e.The average size of Northern Pike is", fish_species_average[4][1])
-    print("1f.The average size of Sunfish is", fish_species_average[5][1])
+    fish_species_average = dict()
 
-    max_num_1 = max(fish_species_average[0][1], fish_species_average[1][1], fish_species_average[2][1],
-                    fish_species_average[3][1], fish_species_average[4][1], fish_species_average[5][1])
+    for i in fish_data_species:
+        fish_species_average[i] = find_average(fish_data_species[i])
+        print("1.The average size of ", i, "is", fish_species_average[i])
 
+    largest_average = 0
+    largest_species = ''
     for i in fish_species_average:
-        largest = i[1]
-        if largest == max_num_1:
-            print("2.The average largest fish is", i[0])
-            print(i)
+        if fish_species_average[i] > largest_average:
+            largest_average = fish_species_average[i]
+            largest_species = i
 
-    # lake with the largest fish
-    average_Compare_lake = [[" Itasca", find_average(itasca)], ["Vermilion", find_average(vermilion)],
-                            ["Snail", find_average(snail)], ["Superior", find_average(superior)],
-                            ["Carlos", find_average(carlos)]]
+    print("2.fish species with Largest average is",largest_species," (",largest_average,")")
 
-    max_num_2 = max(average_Compare_lake[0][1], average_Compare_lake[1][1], average_Compare_lake[2][1],
-                    average_Compare_lake[3][1], find_average(carlos))
-    for i in average_Compare_lake:
-        largest = i[1]
-        if largest == max_num_2:
-            print("3.The average largest fish is found in", i[0])
-            print(i)
+    # average lake---------------------------------------------------
+    # creating a dynamic array for different lake species
+    for i in Counter(lakes).items():
+        fish_data_lake[i[0]] = []
+    # print(fish_data_lake)
+    for i in fish_data:
+        # creating array for different length of different fish species
+        fish_data_lake[i[1]].append(int(i[2]))
+    # print(fish_data_lake)
 
-    # median = statistics.median(sorted(new_length))
-    itasca.sort()
-    median_itasca = statistics.median(sorted(itasca))
-    vermilion.sort()
-    median_vermilion = statistics.median(sorted(vermilion))
-    snail.sort()
-    median_snail = statistics.median(sorted(snail))
-    superior.sort()
-    median_superior = statistics.median(sorted(superior))
-    carlos.sort()
-    median_carlos = statistics.median(sorted(carlos))
-    median_lake = [["Itasca", median_itasca], ['Vermilion', median_vermilion], ['Snail', median_snail],
-                   ["Superior", median_superior], ["Carlos", median_carlos]]
-    highest_median = max(median_itasca, median_vermilion, median_snail, median_superior, median_carlos)
-    for i in median_lake:
-        largest = i[1]
-        if largest == highest_median:
-            print("4.The lake with the largest median (50th percentile) fish", i[0])
-            print(i)
-    # error fixed using changing Length: ['15', '45', '30',.....] to Length: [15, 45, 30] .Error: 'str' to
-    # numerator/ denominator
-    print("------------------------------Date for all fish species "
-          "comparision----------------------------------------------------")
-    new_length = list(map(int, length))
-    # print("1.The average size of all fish species is", average(new_length))
-    print("5.The average highest size of all fish species is", max(new_length))
-    index_of_Largest = new_length.index(max(new_length))
-    print("6.The largest fish species is " + fish_data[index_of_Largest][0])
-    print(fish_data[index_of_Largest])
-    print("7.The lake with the largest fish species is " + fish_data[index_of_Largest][1])
+    lakes_average = dict()
+    for i in fish_data_lake:
+        lakes_average[i] = find_average(fish_data_lake[i])
+        print("3.The average size of ", i, "is", lakes_average[i])
+    # print(lakes_average)
 
-    graphicalRepresentation(fish_Species, new_length, median_lake)
+    largest_lake_average = 0
+    largest_lake = ''
+    for i in lakes_average:
+        if lakes_average[i] > largest_lake_average:
+            largest_lake_average = lakes_average[i]
+            largest_lake = i
+
+    print("Lake ith Largest average is", largest_lake, " (",largest_lake_average, ")")
+
+    lakes_median = dict()
+    for i in fish_data_lake:
+        lakes_median[i] = find_median(fish_data_lake[i])
+        print("4.The median size of ", i, "is", lakes_median[i])
+    # print(lakes_median)
+
+    largest_lake_median = 0
+    largest_lake2 = ''
+    for i in lakes_median:
+        if lakes_median[i] > largest_lake_median:
+            largest_lake_median = lakes_median[i]
+            largest_lake2 = i
+
+    print("4.Lake with Largest median is", largest_lake2, " (", largest_lake_median, ")")
+
+    graphicalRepresentation(fish_Species, length, lakes_median, lakes_average, lakes)
 
 
 def find_average(input_array):
@@ -155,23 +115,44 @@ def find_average(input_array):
     return average
 
 
-def graphicalRepresentation(fish_Species, new_length):
+def find_median(input_array):
+    input_array.sort()
+    # print(input_array)
+    N = len(input_array)
+    median_position = ((N+1)/2)-1
+    if median_position % 2 == 0:
+        median = input_array[int(median_position)]
+    else:
+        median = (input_array[int(median_position-0.5)] + input_array[int(median_position+0.5)])/2
+    # print(statistics.median(input_array))
+    return median
+
+
+def graphicalRepresentation(fish_Species, length, lakes_median, lakes_average,lakes):
     # relation between fishSpecies and length.
-    # the average Largest Fish
-    plt.bar(fish_Species, new_length, color='g', label='File Data')
+    # the average Largest Fish of all species
+    plt.bar(fish_Species, length, color='g', label='File Data')
     plt.xlabel('Fish Species', fontsize=12)
     plt.ylabel('Length in Cm', fontsize=12)
     plt.title('The average Largest of all Fish Species', fontsize=20)
-    plt.legend()
     plt.show()
-    plt.bar(fish_Species, new_length, color='g', label='File Data')
-    plt.xlabel('Fish Species', fontsize=12)
-    plt.ylabel('Length in Cm', fontsize=12)
-    plt.title('The average Largest of all Fish Species', fontsize=20)
-    plt.legend()
+    # Showing The average largest fish is found
+    for i in lakes_average:
+        plt.bar(i, lakes_average[i], color='g', label='File Data')
+        plt.xlabel('Fish Species', fontsize=12)
+        plt.ylabel('Length in Cm', fontsize=12)
+        plt.title('The average largest fish is found', fontsize=20)
     plt.show()
-    # scatter plot showing the length of the fish species
-    plt.scatter(new_length, fish_Species)
+    # Showing The lake with the largest median (50th percentile) fish
+    for i in lakes_median:
+        plt.bar(i, lakes_median[i], color='g', label='File Data')
+        plt.xlabel('Fish Species', fontsize=12)
+        plt.ylabel('Length in Cm', fontsize=12)
+        plt.title('The lake with the largest median (50th percentile) fish', fontsize=20)
+    plt.show()
+
+    # scatter plot showing the length of the all fish species
+    plt.scatter(length, fish_Species)
     plt.show()
 
 
